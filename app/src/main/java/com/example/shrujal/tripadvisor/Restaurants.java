1fc9d414a5a0b9d61386dfb9b233c584f5ec4cee
shrujalr <shrujalraut98@gmail.com>
2017-10-26 22:30:05 +0530

    Second page created

--- app/src/main/java/com/example/shrujal/tripadvisor/Restaurants.java
+++ app/src/main/java/com/example/shrujal/tripadvisor/Restaurants.java
@@ -1,8 +1,20 @@
 package com.example.shrujal.tripadvisor;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
 /**
  * Created by Shrujal on 26-10-2017.
  */
 
-public class Restaurants {
+public class Restaurants extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        TextView textView=new TextView(this);
+        textView.setText("Restaurants Near You ");
+        setContentView(textView);
+    }
 }
