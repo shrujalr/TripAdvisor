3d640a8e0918cdea9dd77dc9fd0b2c9acdf08e53
shrujalr <shrujalraut98@gmail.com>
2017-10-26 22:30:51 +0530

    Second page created

--- app/src/main/java/com/example/shrujal/tripadvisor/Hotels.java
+++ app/src/main/java/com/example/shrujal/tripadvisor/Hotels.java
@@ -1,8 +1,22 @@
 package com.example.shrujal.tripadvisor;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
 /**
  * Created by Shrujal on 26-10-2017.
  */
 
-public class Hotels {
+public class Hotels extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        TextView textView=new TextView(this);
+        textView.setText("Hotels near You");
+        setContentView(textView);
+    }
 }
