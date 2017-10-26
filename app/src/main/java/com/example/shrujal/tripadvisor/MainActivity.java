47e6e311777704a9779be9e19cdd4d58bf96c6fc
shrujalr <shrujalraut98@gmail.com>
2017-10-26 19:11:47 +0530

    Data passing done

--- app/src/main/java/com/example/shrujal/tripadvisor/MainActivity.java
+++ app/src/main/java/com/example/shrujal/tripadvisor/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.shrujal.tripadvisor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnSubmit;
+    EditText etFName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etFName = (EditText) findViewById(R.id.editText3);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(this);
+        }
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, ViewActivity.class);
+        intent.putExtra("fname", etFName.getText().toString());
+        startActivity(intent);
     }
 }
