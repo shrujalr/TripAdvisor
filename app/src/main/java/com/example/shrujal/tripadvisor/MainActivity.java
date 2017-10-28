946db49c3aa5dc2587d2201d69527c8927819447
shrujalr <shrujalraut98@gmail.com>
2017-10-29 01:40:35 +0530

    Scroll View implemented HomePage Added Fonts Changed

--- app/src/main/java/com/example/shrujal/tripadvisor/MainActivity.java
+++ app/src/main/java/com/example/shrujal/tripadvisor/MainActivity.java
@@ -1,15 +1,35 @@
 package com.example.shrujal.tripadvisor;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btnSubmit;
+public class MainActivity extends AppCompatActivity/* implements View.OnClickListener */{
+    TextView t;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        t=(TextView)findViewById(R.id.text1);
+        Typeface mycustomfont=Typeface.createFromAsset(getAssets(),"fonts/alpha_echo.ttf");
+        t.setTypeface(mycustomfont);
+    }
+    public void clickFunction(View v)
+    {
+        Intent intent = new Intent(this, FirstPage.class);
+        startActivity(intent);
+    }
+  /*  Button btnSubmit;
     EditText etFName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +46,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         intent.putExtra("fname", etFName.getText().toString());
 
         startActivity(intent);
-    }
+    }*/
 
 }
